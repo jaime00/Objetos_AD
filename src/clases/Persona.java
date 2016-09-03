cd7b48a8fa12a75d7f47861fadf872c6163b83f1
“Jaime” <jtorres61@cuc.edu.co>
2016-09-03 12:02:32 -0500

    Clase persona creada

--- src/clases/Persona.java
+++ src/clases/Persona.java
@@ -9,6 +9,52 @@ package clases;
  *
  * @author jaime
  */
-public class Persona {
+public class Persona {  
+    private long identificacion;
+    private String primer_nombre;
+    private String primer_apellido;
+    private String sexo;
+    
+    public Persona(long identificacion, String primer_nombre, String primer_apellido){
+    
+        this.identificacion = identificacion;
+        this.primer_nombre = primer_nombre;
+        this.primer_apellido = primer_apellido;
+        this.sexo = sexo;
+        
+    }
+
+    public long getIdentificacion() {
+        return identificacion;
+    }
+
+    public void setIdentificacion(long identificacion) {
+        this.identificacion = identificacion;
+    }
+
+    public String getPrimer_nombre() {
+        return primer_nombre;
+    }
+
+    public void setPrimer_nombre(String primer_nombre) {
+        this.primer_nombre = primer_nombre;
+    }
+
+    public String getPrimer_apellido() {
+        return primer_apellido;
+    }
+
+    public void setPrimer_apellido(String primer_apellido) {
+        this.primer_apellido = primer_apellido;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+    
     
 }
